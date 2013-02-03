08eb7b3f342c7032b922b9acc785c771fbe6f8c9
cdesrosiers <cmdesrosiers@gmail.com>
2013-02-03 02:53:56 -0500

    test commit and added eclipse settings folder to ignore list

--- src/com/example/thamatrix/DreamatrixService.java
+++ src/com/example/thamatrix/DreamatrixService.java
@@ -1,12 +1,17 @@
 package com.example.thamatrix;
 
 import android.service.dreams.DreamService;
+import android.util.Log;
 
 public class DreamatrixService extends DreamService {
+	
+	static final String TAG = "DreamatrixService";
     
 	@Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
+        
+        Log.d(TAG, "onAttachedToWindow");
 
         // Exit dream upon user touch
         setInteractive(false);
